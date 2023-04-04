d4c8ca91ac819205d231f6ceb3cab651eed03a3f
JuninPovoa <rodrigobraule10@gmail.com>
2023-04-04 13:02:26 -0300

    Mudanca no astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,56 +1,6 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-import markdoc from "@astrojs/markdoc";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { defineConfig } from 'astro/config'
 
-import { config as markdocConfig } from './src/utils/mdoc/mdoc.config'
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-
-
-// https://astro.build/config
-
-// https://astro.build/config
-export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
-  // root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // outDir: './dist',       // When running `astro build`, path to final static output
-  // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  output: 'server',
+export default defineConfig({
   site: 'https://jrpovoa.github.io',
-  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   base: '/new-verticals',
-  server: {
-    // port: 3000,         // The port to run the dev server on.
-  },
-  integrations: [mdx(), markdoc(markdocConfig), svelte(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), sitemap()],
-  vite: {
-    plugins: [],
-    resolve: {
-      alias: {
-        '$': path.resolve(__dirname, './src')
-      }
-    },
-    optimizeDeps: {
-      allowNodeBuiltins: true
-    }
-  },
-  adapter: vercel()
-});
+})
