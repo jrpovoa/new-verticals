ce0a636b7c37da4f1ea0386ac43945797c0bc440
JuninPovoa <rodrigobraule10@gmail.com>
2023-04-04 00:23:05 -0300

    commit inicial

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,8 +30,9 @@ export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
   // outDir: './dist',       // When running `astro build`, path to final static output
   // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   output: 'server',
-  site: 'https://astro-ink.vercel.app',
+  site: 'https://jrpovoa.github.io',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  base: '/new-verticals',
   server: {
     // port: 3000,         // The port to run the dev server on.
   },
